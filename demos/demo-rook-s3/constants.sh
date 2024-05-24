a546e942576c81a77c0eaffda75d0958520d8737
MatMol96 <ing.mattiamolinaro@gmail.com>
2024-05-24 18:03:19 +0200

    adjusted demo rook

--- demos/demo-rook-s3/constants.sh
+++ demos/demo-rook-s3/constants.sh
@@ -15,14 +15,14 @@
 # specific language governing permissions and limitations
 # under the License.
 
-AWS_HOST=$(kubectl -n nuvolaris get cm ceph-bucket -o jsonpath='{.data.BUCKET_HOST}') 
-PORT=$(kubectl -n nuvolaris get cm ceph-bucket -o jsonpath='{.data.BUCKET_PORT}') 
-BUCKET_NAME=$(kubectl -n nuvolaris get cm ceph-bucket -o jsonpath='{.data.BUCKET_NAME}') 
-AWS_ACCESS_KEY_ID=$(kubectl -n nuvolaris get secret ceph-bucket -o jsonpath='{.data.AWS_ACCESS_KEY_ID}' | base64 --decode) 
-AWS_SECRET_ACCESS_KEY=$(kubectl -n nuvolaris get secret ceph-bucket -o jsonpath='{.data.AWS_SECRET_ACCESS_KEY}' | base64 --decode) 
+AWS_HOST=$(kubectl -n nuvolaris get cm nuvolaris-ceph-bucket -o jsonpath='{.data.BUCKET_HOST}') 
+PORT=$(kubectl -n nuvolaris get cm nuvolaris-ceph-bucket -o jsonpath='{.data.BUCKET_PORT}') 
+BUCKET_NAME=$(kubectl -n nuvolaris get cm nuvolaris-ceph-bucket -o jsonpath='{.data.BUCKET_NAME}') 
+AWS_ACCESS_KEY_ID=$(kubectl -n nuvolaris get secret nuvolaris-ceph-bucket -o jsonpath='{.data.AWS_ACCESS_KEY_ID}' | base64 --decode) 
+AWS_SECRET_ACCESS_KEY=$(kubectl -n nuvolaris get secret nuvolaris-ceph-bucket -o jsonpath='{.data.AWS_SECRET_ACCESS_KEY}' | base64 --decode) 
 
-echo $AWS_HOST
-echo $PORT
-echo $BUCKET_NAME
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
+echo "AWS_HOST=${AWS_HOST}" >> .env
+echo "PORT=${PORT}" >> .env
+echo "BUCKET_NAME=${BUCKET_NAME}" >> .env
+echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> .env
+echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> .env
